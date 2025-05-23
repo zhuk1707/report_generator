e6d7247595b730277eb2fe4f245ee68f300fd91e
Evgeniy <charizardik64@gmail.com>
2025-05-23 11:46:52 +0300

    fix: report content

--- src/components/ReportContent.jsx
+++ src/components/ReportContent.jsx
@@ -1,140 +1,46 @@
 import React from "react";
 
 export default function ReportContent({formInputs, salesItemInputs, setClipboard}) {
-  let rate = +formInputs.rate
-  if (isNaN(rate)) rate = 0
+  const rate = isNaN(+formInputs.rate) ? 0 : +formInputs.rate;
+  const shopName = {almi: "АЛМИ", mv: "МВ"}[formInputs.shopName] || formInputs.shopName;
 
-  const shopNameFromInput = formInputs.shopName
-  const shopList = {
-    almi: "АЛМИ",
-    mv: 'МВ'
-  }
-  const shopName = shopList[shopNameFromInput]
+  const rentCount = +formInputs.rentCount;
+  const hardware = +formInputs.hardware;
+  const rentProfit = rentCount * +formInputs.rentCost;
+  const baseAmount = rentProfit + hardware;
 
-  const rentCount = +formInputs.rentCount
-  const hardware = +formInputs.hardware
-
-  const rentCost = +formInputs.rentCost
-  const rentProfit = rentCount * rentCost
-  const amount = rentCount * rentCost + hardware
-
-  const sales = salesItemInputs.filter(el => {
-    if (el.price !== 0) {
-      return el
-    }
-  })
-
-  const allAmount = sales.reduce((prev, el) => {
-    return prev + (el.price * el.count)
-  }, amount)
-
-  const allAmountUsd = (allAmount / rate).toFixed(2)
-
-  const renderSales = (sales) => {
-    return sales.map((el, index) => {
-      return <div key={index}>{el.itemName} {el.checkbox ? 'used' : ''} — {el.price}р</div>
+  const sales = salesItemInputs.filter(({price}) => price !== 0);
+  const salesList = sales
+    .map(({itemName, price, count, checkbox}) => {
+      return `${itemName} ${checkbox ? "used" : ""} X ${count} — ${price * count}р`;
     })
-  }
+    .join("\n");
 
-  const getSalesList = (sales) => {
-    return sales.reduce((prev, el) => {
-      return prev + `${el.itemName} ${el.checkbox ? 'used' : ''} — ${el.price}р\n`
-    }, '')
-  }
 
-  if (hardware && sales.length && rentCount) {
-    setClipboard(`Курс = ${rate}\n${shopName}:\n${rentCount} прокат(а/ов) — ${rentProfit}р\n\nПродажи:\n${getSalesList(sales)}\nЖелезо — ${hardware}р\n\nИтого: ${allAmount}р (${allAmountUsd}$)`)
+  const totalAmount = sales.reduce((sum, {price, count}) => sum + price * count, baseAmount);
+  const totalAmountUsd = (totalAmount / rate).toFixed(2);
 
-    return (
-      <div className="report__container">
-        Курс = {rate} <br/>
-        {shopName}:<br/>
-        {rentCount} прокат(а/ов) — {rentProfit}р<br/> <br/>
-        Продажи:
-        {renderSales(sales)} <br/>
-        Железо — {hardware}р<br/><br/>
-        Итого: {allAmount}р ({allAmountUsd}$)<br/>
-      </div>
-    );
-  }
+  const reportClipboard = [`Курс = ${rate}`, `${shopName}:`, `${rentCount} прокат(а/ов) — ${rentProfit}р`];
 
-  if (sales.length && rentCount) {
-    setClipboard(`Курс = ${rate}\n${shopName}:\n${rentCount} прокат(а/ов) — ${rentProfit}р\n\nПродажи:\n${getSalesList(sales)}\nИтого: ${allAmount}р (${allAmountUsd}$)`)
-
-    return (
-      <div className="report__container">
-        Курс = {rate} <br/>
-        {shopName}:<br/>
-        {rentCount} прокат(а/ов) — {rentProfit}р<br/> <br/>
-        Продажи:
-        {renderSales(sales)} <br/>
-        Итого: {allAmount}р ({allAmountUsd}$)<br/>
-      </div>
-    );
-  }
-
-  if (hardware && sales.length) {
-
-    setClipboard(`Курс = ${rate}\n${shopName}:\n${rentCount} прокат(а/ов) — ${rentProfit}р\n\nПродажи:\n${getSalesList(sales)} \nЖелезо — ${hardware}р\nИтого: ${allAmount}р (${allAmountUsd}$)`)
-
-    return (
-      <div className="report__container">
-        Курс = {rate} <br/>
-        {shopName}:<br/>
-        {rentCount} прокат(а/ов) — {rentProfit}р<br/> <br/>
-        Продажи:
-        {renderSales(sales)} <br/>
-        Железо — {hardware}р<br/><br/>
-        Итого: {allAmount}р ({allAmountUsd}$)<br/>
-      </div>
-    );
+  if (sales.length) {
+    reportClipboard.push("\nПродажи:", salesList);
   }
 
   if (hardware) {
-    setClipboard(`Курс = ${rate} \n${shopName}:\n${rentCount} прокат(а/ов) — ${rentProfit}р\nЖелезо — ${hardware}\nИтого: ${allAmount}р (${allAmountUsd}$)`)
-
-    return (
-      <div className="report__container">
-        Курс = {rate} <br/>
-        {shopName}:<br/>
-        {rentCount} прокат(а/ов) — {rentProfit}р<br/>
-        Железо — {hardware}<br/>
-        Итого: {allAmount}р ({allAmountUsd}$)<br/>
-      </div>)
-
+    reportClipboard.push(`\nЖелезо — ${hardware}р`);
   }
 
-  if (sales.length) {
-    setClipboard(`Курс = ${rate}\n${shopName}:\n${rentCount} прокат(а/ов) — ${rentProfit}р\n\nПродажи:\n${getSalesList(sales)}\nИтого: ${allAmount}р (${allAmountUsd}$)`)
-
-    return (
-      <div className='report__container'>
-        Курс = {rate} <br/>
-        {shopName}:<br/>
-        {rentCount} прокат(а/ов) — {rentProfit}р<br/><br/>
-        Продажи:
-        {renderSales(sales)} <br/>
-        Итого: {allAmount}р ({allAmountUsd}$)<br/>
-      </div>
-    )
+  if (sales.length || hardware || rentCount) {
+    reportClipboard.push(`\nИтого: ${totalAmount}р (${totalAmountUsd}$)`);
   }
 
-  if (!rentCount) {
-    setClipboard(`${shopName}: 0\n`)
+  setClipboard(reportClipboard.join("\n"));
 
-    return (
-      <div className="report__container">
-        {shopName}: 0<br/>
-      </div>
-    )
-  }
-
-  setClipboard(`Курс = ${rate}\n${shopName}:\n${rentCount} прокат(а/ов) — ${allAmount}р (${allAmountUsd}$)`)
   return (
     <div className="report__container">
-      Курс = {rate} <br/>
-      {shopName}:<br/>
-      {rentCount} прокат(а/ов) — {allAmount}р ({allAmountUsd}$)
+      {reportClipboard.map((line, index) => (
+        <div key={index}>{line}</div>
+      ))}
     </div>
-  )
+  );
 }
