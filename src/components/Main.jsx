88df5e2b19f1051cc76f410482d518f7eda4624e
Evgeniy <charizardik64@gmail.com>
2025-05-23 12:48:55 +0300

    feat: SalesItem display

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,15 @@
 import Form from "./Form";
 import SalesItem from "./SalesItem";
 import GeneratedReport from "./GeneratedReport";
-import React from "react";
+import React, {useState} from "react";
 import {formsInitialState, salesItems} from "../data";
+import {ChevronDown, ChevronUp} from 'lucide-react'
 
 export default function Main() {
   const [formInputs, setFormInputs] = React.useState(formsInitialState)
-
   const [salesItemInputs, setSalesItemInputs] = React.useState(salesItems)
-
   const [isGenerated, setIsGenerated] = React.useState(false)
+  const [isSalesItemsDisplays, setIsSalesItemsDisplays] = useState(false)
 
   const handleChange = (e) => {
     let key = e.target.name
@@ -44,17 +44,31 @@ export default function Main() {
       <Form labelInfo={'Hardware earnings'} inputInfo={{name: "hardware"}} formInputs={formInputs}
             handleChange={handleChange}/>
 
-      <div className="main__sales sales">
-        {salesItemInputs.map((el, index) => (
-          <SalesItem
-            key={index}
-            index={index}
-            inputs={el}
-            setInputs={setSalesItemInputs}
-            setIsGenerated={setIsGenerated}
-          />
-        ))}
-      </div>
+      <button
+        className='main__button sales-item-display-button'
+        type='button'
+        onClick={() => setIsSalesItemsDisplays(prevState => !prevState)}
+      >{isSalesItemsDisplays
+        ?
+        <><span>Hide</span> <span className='icon'><ChevronUp strokeWidth={1} size={20}/></span></>
+        :
+        <><span>Show Sales Items</span> <span className='icon'><ChevronDown strokeWidth={1} size={20}/></span></>
+      }</button>
+
+      {isSalesItemsDisplays &&
+        <div className="main__sales sales">
+          {salesItemInputs.map((el, index) => (
+            <SalesItem
+              key={index}
+              index={index}
+              inputs={el}
+              setInputs={setSalesItemInputs}
+              setIsGenerated={setIsGenerated}
+            />
+          ))}
+        </div>
+      }
+
 
       <button
         className='main__button generate-button'
