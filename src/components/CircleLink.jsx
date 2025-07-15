922b164a887788e84f105019e504f0554899d2b8
Evgeniy <charizardik64@gmail.com>
2025-07-15 16:52:21 +0300

    fix: link to currency rate

--- src/components/CircleLink.jsx
+++ src/components/CircleLink.jsx
@@ -4,7 +4,7 @@ import myfinLogo from './../assets/myfin-logo.svg'
 export default function CircleLink() {
   return (
     <a className='circle-link'
-       href="https://myfin.by/currency/minsk?utm_source=myfin&utm_medium=organic&utm_campaign=menu&working=0"
+       href="https://myfin.by/currency/minsk?utm_source=myfin&utm_medium=organic&utm_campaign=menu"
        target={"_blank"}
        title={'Go to myfin.by'}
     >
